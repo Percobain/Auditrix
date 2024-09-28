2aa4172a7a2bafac153d8d118a1b347f7a53019d
Shreyans T <shreyans.tatiya@gmail.com>
2024-09-28 13:15:46 +0530

    Add CodeEditor Contract-inputs.tsx Update Styling

--- auditrix/src/app/page.tsx
+++ auditrix/src/app/page.tsx
@@ -2,9 +2,19 @@
 
 import { useState } from "react";
 import Header from "../../components/header";
+import CustomCodeEditor from "../../components/contract-input";
 
 export default function Home() {
+
+  const [contract, setContract] = useState('');
+  const [results, setResults] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const analyze = async () => {}
+
   return <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
       <Header />
+      <CustomCodeEditor analyze={analyze} contract={contract} setContract={setContract}/>
   </main>
 }
