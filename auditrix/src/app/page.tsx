edb5cfa2390b4ac9ca61b8227b676ca5eab6664e
Shreyans T <shreyans.tatiya@gmail.com>
2024-09-28 09:32:08 +0530

    frontend checkpoint1

---
+++ auditrix/src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { useState } from "react";
+import Header from "../../components/header";
+
+export default function Home() {
+  return <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
+      <Header />
+  </main>
+}
